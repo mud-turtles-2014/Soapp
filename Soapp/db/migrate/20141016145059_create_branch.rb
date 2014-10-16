54b872c9ad6b7e435d4f8aa7450d9420c5628795
Anthony F & Rafael Rocha <pair+aferragano.rafaelrochasilva@devbootcamp.com>
2014-10-16 11:59:20 -0400

    Create branch table

--- Soapp/db/migrate/20141016145059_create_branch.rb
+++ Soapp/db/migrate/20141016145059_create_branch.rb
@@ -1,6 +1,9 @@
 class CreateBranch < ActiveRecord::Migration
   def change
     create_table :branches do |t|
+      t.belongs_to :repo
+      t.string :name
+      t.timestamps
     end
   end
 end
