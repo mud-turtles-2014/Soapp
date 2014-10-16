6bfbf16b0ff6bfab4c207619f62744e715bf35bf
Anthony F & Rafael Rocha <pair+aferragano.rafaelrochasilva@devbootcamp.com>
2014-10-16 12:48:32 -0400

    fix db. models. migrations. functional db

--- Soapp/db/migrate/20141016155021_create_file_changes.rb
+++ Soapp/db/migrate/20141016155021_create_file_changes.rb
@@ -2,7 +2,7 @@ class CreateFileChanges < ActiveRecord::Migration
   def change
     create_table :file_changes do |t|
       t.belongs_to :commit
-      t.text :changes
+      t.text :change
       t.timestamps
     end
   end
