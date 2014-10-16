ee110da7a953c7d366cfb6e7a34f9daa3429efcc
Anthony F & Rafael Rocha <pair+aferragano.rafaelrochasilva@devbootcamp.com>
2014-10-16 10:48:03 -0400

    Initialize skeleton

---
+++ Soapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soapp::Application.config.secret_key_base = 'acee94e9f41b146e026ca52bb82875f2546b4ae666b954349f44d55399c1046db0598543fe1d43230a333cbc4ee7aef9722918f8838ea85115d59b5167a2d183'
