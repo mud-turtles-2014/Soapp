338abab70f480cdeba96a54e4f2a7005717ea2a0
chris wallwork <chris.wallwork@gmail.com>
2014-10-16 17:55:58 -0400

    hook test

--- Soapp/config/routes.rb
+++ Soapp/config/routes.rb
@@ -1,6 +1,6 @@
 Soapp::Application.routes.draw do
   resources :commits
-  post "make-commit" => 'commits#make_commit'
+  post "/make-commit" => 'commits#make_commit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
