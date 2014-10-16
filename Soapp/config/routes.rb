0bede4dbf69d038c4d941e6212bf5e19e29a65d5
chris wallwork <chris.wallwork@gmail.com>
2014-10-16 18:07:49 -0400

    hook test

--- Soapp/config/routes.rb
+++ Soapp/config/routes.rb
@@ -3,7 +3,7 @@ Soapp::Application.routes.draw do
   post "/make_commit" => 'commits#make_commit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-#comment
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
