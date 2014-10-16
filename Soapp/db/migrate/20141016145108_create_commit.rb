e96b1572904f0e8517547bd8c41f6758a18bb0bf
Anthony F & Rafael Rocha <pair+aferragano.rafaelrochasilva@devbootcamp.com>
2014-10-16 11:58:40 -0400

    Create Commit table

--- Soapp/db/migrate/20141016145108_create_commit.rb
+++ Soapp/db/migrate/20141016145108_create_commit.rb
@@ -1,6 +1,9 @@
 class CreateCommit < ActiveRecord::Migration
   def change
     create_table :commits do |t|
+      t.belongs_to :branch
+      t.string :message
+      t.timestamps
     end
   end
 end
