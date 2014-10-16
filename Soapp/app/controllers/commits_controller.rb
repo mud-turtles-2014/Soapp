529ab0242afc9d383bf42bdf354bf10078eeecfb
chris wallwork <chris.wallwork@gmail.com>
2014-10-16 17:30:10 -0400

    added basic commit controller

---
+++ Soapp/app/controllers/commits_controller.rb
@@ -0,0 +1,19 @@
+class CommitsController < ApplicationController
+
+before_action :get_commit, except: :create
+
+  def create
+    commit_hash = JSON.parse(params.body)
+    Commit.create(commit_hash)
+  end
+
+  def get_commit
+    @commit = Commit.find_by(sha: sha)
+  end
+
+  def show
+
+  end
+
+
+end
