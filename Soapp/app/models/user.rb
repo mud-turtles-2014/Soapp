42477227f58feea37e3d064ab06b1bb0c44b0e6f
Anthony F & Rafael Rocha <pair+aferragano.rafaelrochasilva@devbootcamp.com>
2014-10-16 20:56:08 -0400

    Add new validations and refactor create_with_omniauth

--- Soapp/app/models/user.rb
+++ Soapp/app/models/user.rb
@@ -2,15 +2,22 @@ class User < ActiveRecord::Base
   has_many :team_projects
   has_many :repos, through: :team_projects
 
-  validates_presence_of :github_uid, :name, :github_token
-  validates_uniqueness_of :github_uid, :display_name
+  validates_presence_of :uid, :name, :email, :token
+  validates_uniqueness_of :email, :uid, :token
 
-  # def self.create_with_omniauth(auth)
-  #   create! do |user|
-  #     user.provider = auth["provider"]
-  #     user.uid = auth["uid"]
-  #     user.name = auth["info"]["name"]
-  #   end
-  # end
+  def self.from_omniauth(auth)
+    where(auth.slice('email')).first || create_with_omniauth(auth)
+  end
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.uid = auth["uid"]
+      user.token = auth["credentials"]["token"]
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+
+      user.avatar_url = auth["info"]["image"]
+    end
+  end
 
 end
