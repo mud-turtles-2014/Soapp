220bd910c34c72fd84796e10b0621b09f1701e48
chris wallwork <chris.wallwork@gmail.com>
2014-10-17 11:06:36 -0400

    add special params for email

--- Soapp/app/controllers/commits_controller.rb
+++ Soapp/app/controllers/commits_controller.rb
@@ -4,8 +4,8 @@ before_action :get_commit, except: :create_commit
 
   def create_commit
     @branch = Branch.find_or_create_by(name: commit_params[:branch])
+    @user = User.find_by(email: commit_email_params[:email])
     @branch.commits << Commit.create(commit_params)
-
   end
 
   def get_commit
@@ -18,7 +18,11 @@ before_action :get_commit, except: :create_commit
 
   private
   def commit_params
-    params.require(:commit).permit(:sha,:email,:branch,:diff,:commit_msg)
+    params.require(:commit).permit(:sha, :branch,:diff,:commit_msg)
+    # params.require(:commit).permit(:message)
+  end
+  def commit_email_params
+    params.require(:commit).permit(:email)
     # params.require(:commit).permit(:message)
   end
 #comment
