a785db00c8575f033d7e2289e41e2a3a915fa8af
Rafael Rocha <null>
2014-10-17 13:50:59 -0400

    create soapp list area

--- Soapp/app/controllers/repos_controller.rb
+++ Soapp/app/controllers/repos_controller.rb
@@ -3,8 +3,10 @@ class ReposController < ApplicationController
   def index
     @user = User.find(session[:user_id])
     @github = Github.new  oauth_token: @user.token
-
     @repos = @github.repos.list
+
+    @soapps = Repo.all
+    # @soapps = @user.repo
   end
 
   def new
@@ -14,7 +16,7 @@ class ReposController < ApplicationController
   def create
   	@repo = Repo.new(repo_params)
   	if @repo.save
-  		redirect_to repo_path(@repo)
+  		redirect_to repos_path
   	else
   		redirect_to new_repo_path
   	end
