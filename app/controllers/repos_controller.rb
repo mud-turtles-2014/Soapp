5a2d0e78b6b73942f1596ccefbbea2e24f728031
Anthony Ferragano <aferragano@gmail.com>
2014-10-23 01:23:33 -0400

    reverting to old code

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -16,11 +16,11 @@
     @github_repos = get_github_repos
     @repo = Repo.new
 
-    respond_to do |format|
-      format.html{render 'index.html.erb'}
-      format.json{render 'index.json.jbuilder'}
-    end
-
+    # respond_to do |format|
+    #   format.html{render 'index.html.erb'}
+    #   format.json{render 'index.json.jbuilder'}
+    # end
+    render 'index.html.erb'
   end
 
   def heat_map(repo_commits)
