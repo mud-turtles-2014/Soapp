0c93c2ebe23688894cf90fc5e3df52a3591712fd
Anthony Ferragano <aferragano@gmail.com>
2014-10-23 01:38:15 -0400

    moving interval

--- app/assets/javascripts/repo_view/init_app.js
+++ app/assets/javascripts/repo_view/init_app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
   var intervalNewCommits;
-  intervalNewCommits = setInterval(function() { Repo.getNewCommits() }, 3000);
+  
   $(".all_repos").ready(function(){
     $('.add_button').click(function(e){
       e.preventDefault();
       View.toggle_new_btn();
     });
-
+intervalNewCommits = setInterval(function() { Repo.getNewCommits() }, 3000);
     $('.btn-delete').on('ajax:success', function(data) {
       $(this).parent().remove();
     });
