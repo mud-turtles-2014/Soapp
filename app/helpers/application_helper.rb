107460ecf315f46e374ac64376da7068df145ece
Anthony Ferragano <aferragano@gmail.com>
2014-10-26 11:15:20 -0400

    comment out function, comments in if else collisions logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def find_collisions(repo)
-    files = []
-    repo.commits.each do |commit|
-      files << commit.path_names
-    end
-    files.detect{ |change| files.count(change) > 1}
-  end
+  # def find_collisions(repo)
+  #   files = []
+  #   repo.commits.each do |commit|
+  #     files << commit.path_names
+  #   end
+  #   files.detect{ |change| files.count(change) > 1}
+  # end
 
   def form_time(time)
     time.strftime("%a %m/%d/%y %H:%M")
